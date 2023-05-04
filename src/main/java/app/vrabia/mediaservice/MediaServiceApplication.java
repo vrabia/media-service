27b314b45e9439d4bd5077afd6972d9ac235365e
Lorena Arcalean <arcalean.lorena22@gmail.com>
2023-05-04 21:18:23 +0300

    remove pushed file

--- src/main/java/app/vrabia/mediaservice/MediaServiceApplication.java
+++ src/main/java/app/vrabia/mediaservice/MediaServiceApplication.java
@@ -3,7 +3,7 @@ package app.vrabia.mediaservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "app.vrabia")
 public class MediaServiceApplication {
 
     public static void main(String[] args) {
